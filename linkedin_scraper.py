cb1731d48cbb49861f1d2ca3c100d71c82e09311
byrontung <55170214+byrontung@users.noreply.github.com>
2020-01-18 16:23:23 -0700

    Add files via upload
    Cleaned up and modulated code.

--- linkedin_scraper.py
+++ linkedin_scraper.py
@@ -3,36 +3,50 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def main1():
+def get_html():
     html_doc = "https://www.linkedin.com/jobs/search/?keywords=software%20engineer"
     page = requests.get(html_doc)
     soup = BeautifulSoup(page.content, "html.parser")
-    # print(soup)
+    return soup
+
+
+def get_company_list(companies):
+    temp_company_list = []
+    for i in companies:
+        temp_company_list.append(i.text)
+    return temp_company_list
+
+
+def get_position_list(positions):
+    temp_position_list = []
+    for i in positions:
+        temp_position_list.append(i.text)
+    return temp_position_list
+
+
+def info_format(l_companies, l_positions):
     info = []
+    for x in range(len(l_companies)):
+        info.append([l_companies[x], l_positions[x]])
+    return info
+
+
+def main1():
+    # info = []
+    soup = get_html()
     positions = soup.find_all("a", {"class": "result-card__full-card-link"})
-    # for i in position:
-    #     print(i.text)
     companies = soup.find_all(
         "a", {"class": "result-card__subtitle-link job-result-card__subtitle-link"})
 
-    company_list = []
-    for i in companies:
-        company_list.append(i.text)
+    company_list = get_company_list(companies)
 
-    position_list = []
-    for i in positions:
-        position_list.append(i.text)
+    position_list = get_position_list(positions)
 
-    for x in range(len(companies)):
-        info.append([company_list[x], position_list[x]])
+    finfo = info_format(company_list, position_list)
 
-    return(info)
+    return finfo
 
 
 if __name__ == "__main__":
     main1()
-# for inc in range(len(companies)):
-#     combined = [companies[inc], positions[inc]]
-#     info.append(combined)
-
-# print(info)
+    print(main1())

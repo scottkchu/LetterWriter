90caa499a3cbf1fb3ade157b510be6b5c347b754
byrontung <55170214+byrontung@users.noreply.github.com>
2020-01-18 16:07:33 -0700

    Add files via upload
    Runs on main1(). Return a list of lists.

--- linkedin_scraper.py
+++ linkedin_scraper.py
@@ -2,47 +2,37 @@ from bs4 import BeautifulSoup
 # from urllib.request import urlopen
 import requests
 
-html_doc = "https://www.linkedin.com/jobs/search/?keywords=software%20engineer"
 
-page = requests.get(html_doc)
-soup = BeautifulSoup(page.content, "html.parser")
-# print(soup)
-position = soup.find_all("a", {"class": "result-card__full-card-link"})
-for i in positions:
-    print(i.text)
-companies = soup.find_all(
-    "a", {"class": "result-card__subtitle-link job-result-card__subtitle-link"})
-# for i in companies:
-#     print(i.text)
-# second = soup.find({'class': "jobs-search-results__list artdeco-list"})
-# # print(soup.find(class_="jobs-search-results__list artdeco-list"))
-# print(first)
-# print(second)
-# items = []
+def main1():
+    html_doc = "https://www.linkedin.com/jobs/search/?keywords=software%20engineer"
+    page = requests.get(html_doc)
+    soup = BeautifulSoup(page.content, "html.parser")
+    # print(soup)
+    info = []
+    positions = soup.find_all("a", {"class": "result-card__full-card-link"})
+    # for i in position:
+    #     print(i.text)
+    companies = soup.find_all(
+        "a", {"class": "result-card__subtitle-link job-result-card__subtitle-link"})
 
-# for i in range(5):
+    company_list = []
+    for i in companies:
+        company_list.append(i.text)
 
-# test = soup.find_all(
-# "div", {'class': "jobs-details-top-card__content-container"})
-# print(test)
-# test1 = soup.find_all("data-job-id")
-# print(test1)
-# for i in test1:
-#     print(i)
+    position_list = []
+    for i in positions:
+        position_list.append(i.text)
 
-# cnp = soup.find_all(class_="display-flex")
+    for x in range(len(companies)):
+        info.append([company_list[x], position_list[x]])
 
-# print(cnp)
-
-# items = companynamepos.find_all(class_="display-flex")
-# print(items)
+    return(info)
 
 
-# info = clean_page.find_all(class_="jobs-details-top-card__content-container")
-# print(tryfind)
-
-
-# info = soup.find_all(class_="jobs-details-top-card__content-container")
+if __name__ == "__main__":
+    main1()
+# for inc in range(len(companies)):
+#     combined = [companies[inc], positions[inc]]
+#     info.append(combined)
 
 # print(info)
-# print(soup.find_all(class_="jobs-details-top-card__job-title t-20 t-black t-normal"))
